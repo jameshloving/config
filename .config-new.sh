a6b8e4e65faf5bf6c2b7094b59f60cfc46ad3860
James Loving <jameshloving@gmail.com>
2017-01-26 13:48:42 -0500

    Add neovim aliases for new systems only

--- .config-new.sh
+++ .config-new.sh
@@ -5,3 +5,6 @@ mv .config/nvim/init.vim ~/.config/nvim/init.vim
 echo '' >> ~/.tmux.conf
 echo '#enable mouse scrolling' >> ~/.tmux.conf
 echo 'set -g mouse on' >> ~/.tmux.conf
+echo '\n#neovim aliases' >> ~/.bashrc
+echo "alias vim='nvim'" >> ~/.bashrc
+echo "alias vi='nvim'" >> ~/.bashrc
